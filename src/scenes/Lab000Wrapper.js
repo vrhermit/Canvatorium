211574111f6c1e2c7c3ea56ed5063ff10668bb82
Joe Simpson <joe@radicalappdev.com>
2022-01-18 15:18:14 -0500

    Adding some colors and some cubes to test the colors in VR

--- src/scenes/Lab000Wrapper.js
+++ src/scenes/Lab000Wrapper.js
@@ -1,5 +1,5 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, Color3, Vector3, MeshBuilder, StandardMaterial } from "@babylonjs/core";
-
+import { Engine, Scene, ArcRotateCamera, HemisphericLight, Color3, Vector3, Mesh, MeshBuilder, StandardMaterial } from "@babylonjs/core";
+import SceneColors from "../helpers/SceneColors";
 const Lab000Wrapper = {
   engine: null,
   scene: null,
@@ -23,15 +23,42 @@ const Lab000Wrapper = {
     ambientLight1.intensity = 0.7;
     const ambientLight2 = new HemisphericLight("light-02", new Vector3(-5, 5, -5), scene);
     ambientLight2.intensity = 0.7;
-    scene.clearColor = new Color3(0.9, 0.9, 0.9);
+    scene.clearColor = SceneColors.light4;
+    // scene.clearColor = new Color3(0.9, 0.9, 0.9);
 
     // Add a ground plane to the scene. Used for WebXR teleportation
     const ground = MeshBuilder.CreateGround("ground", { height: 50, width: 60, subdivisions: 4 }, scene);
     const groundMat = new StandardMaterial("ground-material", scene);
     groundMat.alpha = 1;
-    groundMat.diffuseColor = new Color3(0.6, 0.6, 0.6);
+    groundMat.diffuseColor = SceneColors.dark1;
+    // groundMat.diffuseColor = new Color3(0.6, 0.6, 0.6);
     groundMat.specularColor = new Color3(0.2, 0.2, 0.2);
     ground.material = groundMat;
+
+    // Make some boxes to test out the colors in VR
+    const group = new Mesh("logo-group");
+    group.position = new Vector3(0, 1.2, 3);
+    // group.rotation = new Vector3(0, 2, 0);
+    group.scaling = new Vector3(0.3, 0.3, 0.3);
+    makeBox("purple", group, scene).position = new Vector3(0, 0, 0);
+    makeBox("blue", group, scene).position = new Vector3(0.5, 0, 0);
+    makeBox("teal", group, scene).position = new Vector3(1.0, 0, 0);
+    makeBox("cyan", group, scene).position = new Vector3(1.5, 0, 0);
+
+    makeBox("green", group, scene).position = new Vector3(2.0, 0, 0);
+    makeBox("yellow", group, scene).position = new Vector3(2.5, 0, 0);
+    makeBox("orange", group, scene).position = new Vector3(3.0, 0, 0);
+    makeBox("red", group, scene).position = new Vector3(3.5, 0, 0);
+
+    makeBox("dark1", group, scene).position = new Vector3(0, 1, 0);
+    makeBox("dark2", group, scene).position = new Vector3(0.5, 1, 0);
+    makeBox("dark3", group, scene).position = new Vector3(1.0, 1, 0);
+    makeBox("dark4", group, scene).position = new Vector3(1.5, 1, 0);
+
+    makeBox("light1", group, scene).position = new Vector3(2.0, 1, 0);
+    makeBox("light2", group, scene).position = new Vector3(2.5, 1, 0);
+    makeBox("light3", group, scene).position = new Vector3(3.0, 1, 0);
+    makeBox("light4", group, scene).position = new Vector3(3.5, 1, 0);
 
     // WebXRDefaultExperience
     const xrDefault = scene.createDefaultXRExperienceAsync({
@@ -46,6 +73,17 @@ const Lab000Wrapper = {
   }
 
   // --- Call these functions from Vue to pass in the data or setup the scene ---
+};
+
+const makeBox = (colorName, parent, scene) => {
+  // Create a colored box from using a string to get the color from the Brand object
+  const mat = new StandardMaterial(`${colorName}-material`, scene);
+  mat.diffuseColor = SceneColors[colorName];
+  mat.specularColor = new Color3(0.1, 0.1, 0.1);
+  const mesh = MeshBuilder.CreateBox(`${colorName}-box`, { size: 0.5 }, scene);
+  mesh.material = mat;
+  mesh.parent = parent;
+  return mesh;
 };
 
 export default Lab000Wrapper;
