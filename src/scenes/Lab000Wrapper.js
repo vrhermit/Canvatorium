664e56265e4eb1b92cbedf1758ee16f86b3be038
Joseph Simpson <joe@radicalappdev.com>
2022-01-20 13:56:55 -0500

    Cleaning up lab000

--- src/scenes/Lab000Wrapper.js
+++ src/scenes/Lab000Wrapper.js
@@ -1,6 +1,6 @@
 import * as BABYLON from "babylonjs";
+import * as GUI from "babylonjs-gui";
 
-import LabColors from "../lab-shared/LabColors";
 import addLabCamera from "../lab-shared/LabCamera";
 import addLabLights from "../lab-shared/LabLights";
 import addLabRoom from "../lab-shared/LabRoom";
@@ -23,25 +23,7 @@ const Lab000Wrapper = {
     const group = new BABYLON.Mesh("logo-group");
     group.position = new BABYLON.Vector3(-3.5, 0.5, 0);
 
-    makeBox("purple", group, scene).position = new BABYLON.Vector3(0, 0, 0);
-    makeBox("blue", group, scene).position = new BABYLON.Vector3(1, 0, 0);
-    makeBox("teal", group, scene).position = new BABYLON.Vector3(2, 0, 0);
-    makeBox("cyan", group, scene).position = new BABYLON.Vector3(3, 0, 0);
-
-    makeBox("green", group, scene).position = new BABYLON.Vector3(4, 0, 0);
-    makeBox("yellow", group, scene).position = new BABYLON.Vector3(5, 0, 0);
-    makeBox("orange", group, scene).position = new BABYLON.Vector3(6, 0, 0);
-    makeBox("red", group, scene).position = new BABYLON.Vector3(7, 0, 0);
-
-    makeBox("dark1", group, scene).position = new BABYLON.Vector3(0, 1, 0);
-    makeBox("dark2", group, scene).position = new BABYLON.Vector3(1, 1, 0);
-    makeBox("dark3", group, scene).position = new BABYLON.Vector3(2, 1, 0);
-    makeBox("dark4", group, scene).position = new BABYLON.Vector3(3, 1, 0);
-
-    makeBox("light1", group, scene).position = new BABYLON.Vector3(4, 1, 0);
-    makeBox("light2", group, scene).position = new BABYLON.Vector3(5, 1, 0);
-    makeBox("light3", group, scene).position = new BABYLON.Vector3(6, 1, 0);
-    makeBox("light4", group, scene).position = new BABYLON.Vector3(7, 1, 0);
+    makeCard();
 
     // WebXRDefaultExperience
     const xrDefault = scene.createDefaultXRExperienceAsync({
@@ -58,15 +40,21 @@ const Lab000Wrapper = {
   // --- Call these functions from Vue to pass in the data or setup the scene ---
 };
 
-const makeBox = (colorName, parent, scene) => {
-  // Create a colored box from using a string to get the color from the Brand object
-  const mat = new BABYLON.StandardMaterial(`${colorName}-material`, scene);
-  mat.diffuseColor = LabColors[colorName];
-  mat.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
-  const mesh = BABYLON.MeshBuilder.CreateBox(`${colorName}-box`, { size: 1 }, scene);
-  mesh.material = mat;
-  mesh.parent = parent;
-  return mesh;
+const makeCard = () => {
+  // GUI
+  var plane = BABYLON.MeshBuilder.CreatePlane("plane");
+  plane.position.y = 1;
+
+  var advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+  advancedTexture.name = "card-texture";
+
+  var cardText = new GUI.TextBlock("card-text");
+  cardText.text = "Canvatorium";
+  cardText.color = "white";
+  cardText.fontSize = 64;
+
+  advancedTexture.addControl(cardText);
+  plane.scaling = new BABYLON.Vector3(5, 5, 5);
 };
 
 export default Lab000Wrapper;
