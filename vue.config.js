3f74c22b00adb0a2739088790e903b3b762aaf3d
Joe Simpson <97741130+vrhermit@users.noreply.github.com>
2022-02-11 08:49:23 -0500

    removing https from dev server of deployment branch

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,7 @@ module.exports = {
     open: process.platform === "darwin",
     host: "0.0.0.0",
     port: 8080,
-    https: {
-      key: fs.readFileSync(".certs/key.pem"),
-      cert: fs.readFileSync(".certs/cert.pem")
-    },
+
     hotOnly: false
   }
 };
