70bf190a7064f5b8f7024c60ba461291bd1476d3
Joe Simpson <97741130+vrhermit@users.noreply.github.com>
2022-02-11 08:29:46 -0500

    adding dist subtree

---
+++ js/about.39aef87c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var a=t("7a23"),c={class:"about"},o=Object(a["createElementVNode"])("h1",null,"This is an about page",-1),r=[o];function s(e,n){return Object(a["openBlock"])(),Object(a["createElementBlock"])("div",c,r)}var u=t("6b0d"),b=t.n(u);const i={},l=b()(i,[["render",s]]);n["default"]=l}}]);
+//# sourceMappingURL=about.39aef87c.js.map
