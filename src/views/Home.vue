9b82a6fa40c7763d493281a23c9cf62fe5468af2
Joe Simpson <97741130+vrhermit@users.noreply.github.com>
2022-02-13 13:57:44 -0500

    Minor changes to the site structure.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,52 @@
 <template>
-  <article class="home">
-    <p>Everything about this project is a work in progress.</p>
-    <p>
-      This loosely structured project is a place where I can prototype and
-      experiment with UI/UX for spatial interfaces. Some of my ideas include:
-    </p>
+  <div class="home">
+    <article>
+      <h3>A few notes</h3>
+      <p>Everything about this project is a work in progress.</p>
+      <p>
+        This loosely structured project is a place where I can prototype and
+        experiment with UI/UX for spatial interfaces. Some of my ideas include:
+      </p>
 
-    <ul>
-      <li>Using Babylon JS with the Vue 3 Composition API and script setup</li>
-      <li>Reactive data on Babylon JS GUI</li>
-      <li>Reactive/interactive controls with Babylon JS GUI</li>
-      <li>
-        Laying out data in 3D Building controls for common data editing tasks
-      </li>
-      <li>
-        Navigating data as if each record were an object in 3D Exploring
-        productive workflows
-      </li>
-      <li>
-        Replacing some of the flat-screen work that I do in various apps and
-        sites
-      </li>
-      <li>Working with various APIs and databases</li>
-      <li>Detailing and documenting common Babylon GUI workflows</li>
-    </ul>
+      <ul>
+        <li>
+          Using Babylon JS with the Vue 3 Composition API and script setup
+        </li>
+        <li>Reactive data on Babylon JS GUI</li>
+        <li>Reactive/interactive controls with Babylon JS GUI</li>
+        <li>
+          Laying out data in 3D Building controls for common data editing tasks
+        </li>
+        <li>
+          Navigating data as if each record were an object in 3D Exploring
+          productive workflows
+        </li>
+        <li>
+          Replacing some of the flat-screen work that I do in various apps and
+          sites
+        </li>
+        <li>Working with various APIs and databases</li>
+        <li>Detailing and documenting common Babylon GUI workflows</li>
+      </ul>
 
-    <p>
-      The unit of account for this project is the <em>lab</em>. Each lab is a
-      place where I can explore a concept or idea. In Vue terms, each lab will
-      be a page or component that we can navigate to. I won’t write a post for
-      every lab.
-    </p>
-    <p>Each lab contains a Babylon JS scene, a title, and some notes.</p>
-    <p>
-      Check the code for each lap for more details. The repo is
-      <a href="https://github.com/vrhermit/Canvatorium">here</a>
-    </p>
-    <p>
-      I have a GitHub Project
-      <a href="https://github.com/users/vrhermit/projects/3/views/4">Board</a>
-      setup that I use to track the status of each lab and idea.
-    </p>
-  </article>
+      <p>
+        The unit of account for this project is the <em>lab</em>. Each lab is a
+        place where I can explore a concept or idea. In Vue terms, each lab will
+        be a page or component that we can navigate to. I won’t write a post for
+        every lab.
+      </p>
+      <p>Each lab contains a Babylon JS scene, a title, and some notes.</p>
+      <p>
+        Check the code for each lap for more details. The repo is
+        <a href="https://github.com/vrhermit/Canvatorium">here</a>
+      </p>
+      <p>
+        I have a GitHub Project
+        <a href="https://github.com/users/vrhermit/projects/3/views/4">Board</a>
+        setup that I use to track the status of each lab and idea.
+      </p>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -56,5 +61,8 @@ export default {
   padding: 1rem;
   margin: 1rem;
   height: calc(100vh - 5rem);
+}
+article {
+  margin-top: 0%;
 }
 </style>
