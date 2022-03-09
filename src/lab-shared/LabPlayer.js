df093ad105bcadba9198ab86a169cfa87635f6ab
Joseph Simpson <joe@radicalappdev.com>
2022-03-09 13:28:27 -0500

    Working on lap player movement.

--- src/lab-shared/LabPlayer.js
+++ src/lab-shared/LabPlayer.js
@@ -7,7 +7,7 @@ Current features
 - Imports and uses LabConsole for debugging in VR
 */
 
-// import * as BABYLON from "babylonjs";
+import * as BABYLON from "babylonjs";
 // import { ref, reactive, watch } from "@vue/runtime-core");
 import { createLabMenu } from "../lab-shared/LabMenu";
 
@@ -16,6 +16,7 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
   // Create the default experience
   let xr = await scene.createDefaultXRExperienceAsync({
     floorMeshes: teleportMeshes,
+    disableTeleportation: true,
     pointerSelectionOptions: {
       enablePointerSelectionOnAllControllers: true
     }
@@ -25,6 +26,52 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
   xr.baseExperience.onInitialXRPoseSetObservable.add((xrCamera) => {
     xrCamera.position.z = -2;
   });
+
+  // const xrInput = xr.input;
+  const swappedHandednessConfiguration = [
+    {
+      allowedComponentTypes: [BABYLON.WebXRControllerComponent.THUMBSTICK_TYPE, BABYLON.WebXRControllerComponent.TOUCHPAD_TYPE],
+      forceHandedness: "right",
+      axisChangedHandler: (axes, movementState, featureContext, xrInput) => {
+        console.log(xrInput);
+        movementState.rotateX = Math.abs(axes.x) > featureContext.rotationThreshold ? axes.x : 0;
+        movementState.rotateY = Math.abs(axes.y) > featureContext.rotationThreshold ? axes.y : 0;
+      }
+    },
+    {
+      allowedComponentTypes: [BABYLON.WebXRControllerComponent.THUMBSTICK_TYPE, BABYLON.WebXRControllerComponent.TOUCHPAD_TYPE],
+      forceHandedness: "left",
+      axisChangedHandler: (axes, movementState, featureContext, xrInput) => {
+        console.log(xrInput);
+        movementState.moveX = Math.abs(axes.x) > featureContext.movementThreshold ? axes.x : 0;
+        movementState.moveY = Math.abs(axes.y) > featureContext.movementThreshold ? axes.y : 0;
+      }
+    }
+  ];
+
+  function setupCameraForCollisions(camera) {
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
+    camera.ellipsoid = new BABYLON.Vector3(0.7, 1, 0.7);
+    camera.ellipsoidOffset = new BABYLON.Vector3(0, 0.5, 0);
+  }
+
+  setupCameraForCollisions(xr.input.xrCamera);
+
+  const featureManager = xr.baseExperience.featuresManager;
+
+  const movementFeature = featureManager.enableFeature(BABYLON.WebXRFeatureName.MOVEMENT, "latest", {
+    xrInput: xr.input,
+    customRegistrationConfigurations: swappedHandednessConfiguration,
+
+    // add options here
+    movementOrientationFollowsViewerPose: true // default true
+  });
+  movementFeature.movementSpeed = 0.5;
+  // movementFeature.rotationEnabled = false;
+  // movementFeature.rotationSpeed = 2;
+
+  console.log(movementFeature);
 
   // const featuresManager = xr.featuresManager;
   // featuresManager.enableFeature(BABYLON.WebXRFeatureName.POINTER_SELECTION, "stable", {
@@ -39,19 +86,19 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
       if (motionController.handness === "left") {
         const xr_ids = motionController.getComponentIds();
         let triggerComponent = motionController.getComponent(xr_ids[0]); //xr-standard-trigger
-        triggerComponent.onButtonStateChangedObservable.add(() => {
+        triggerComponent?.onButtonStateChangedObservable.add(() => {
           if (triggerComponent.pressed) {
             console.log("Left Trigger Pressed");
           }
         });
         let squeezeComponent = motionController.getComponent(xr_ids[1]); //xr-standard-squeeze
-        squeezeComponent.onButtonStateChangedObservable.add(() => {
+        squeezeComponent?.onButtonStateChangedObservable.add(() => {
           if (squeezeComponent.pressed) {
             console.log("Left Grip Pressed");
           }
         });
         let thumbstickComponent = motionController.getComponent(xr_ids[2]); //xr-standard-thumbstick
-        thumbstickComponent.onButtonStateChangedObservable.add(() => {
+        thumbstickComponent?.onButtonStateChangedObservable.add(() => {
           if (thumbstickComponent.pressed) {
             console.log("Left Thumbstick Pressed");
           }
@@ -61,13 +108,13 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
         // });
 
         let xButtonComponent = motionController.getComponent(xr_ids[3]); //x-button
-        xButtonComponent.onButtonStateChangedObservable.add(() => {
+        xButtonComponent?.onButtonStateChangedObservable.add(() => {
           if (xButtonComponent.pressed) {
             console.log("X Button Pressed");
           }
         });
         let yButtonComponent = motionController.getComponent(xr_ids[4]); //y-button
-        yButtonComponent.onButtonStateChangedObservable.add(() => {
+        yButtonComponent?.onButtonStateChangedObservable.add(() => {
           if (yButtonComponent.pressed) {
             console.log("Y Button Pressed");
             toggleMenu(controller);
@@ -80,19 +127,19 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
       if (motionController.handness === "right") {
         const xr_ids = motionController.getComponentIds();
         let triggerComponent = motionController.getComponent(xr_ids[0]); //xr-standard-trigger
-        triggerComponent.onButtonStateChangedObservable.add(() => {
+        triggerComponent?.onButtonStateChangedObservable.add(() => {
           if (triggerComponent.pressed) {
             console.log("Right Trigger Pressed");
           }
         });
         let squeezeComponent = motionController.getComponent(xr_ids[1]); //xr-standard-squeeze
-        squeezeComponent.onButtonStateChangedObservable.add(() => {
+        squeezeComponent?.onButtonStateChangedObservable.add(() => {
           if (squeezeComponent.pressed) {
             console.log("Right Grip Pressed");
           }
         });
         let thumbstickComponent = motionController.getComponent(xr_ids[2]); //xr-standard-thumbstick
-        thumbstickComponent.onButtonStateChangedObservable.add(() => {
+        thumbstickComponent?.onButtonStateChangedObservable.add(() => {
           if (thumbstickComponent.pressed) {
             console.log("Right Thumbstick Pressed");
           }
@@ -102,13 +149,13 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
         // });
 
         let aButtonComponent = motionController.getComponent(xr_ids[3]); //a-button
-        aButtonComponent.onButtonStateChangedObservable.add(() => {
+        aButtonComponent?.onButtonStateChangedObservable.add(() => {
           if (aButtonComponent.pressed) {
             console.log("A Button Pressed");
           }
         });
         let bButtonComponent = motionController.getComponent(xr_ids[4]); //b-button
-        bButtonComponent.onButtonStateChangedObservable.add(() => {
+        bButtonComponent?.onButtonStateChangedObservable.add(() => {
           if (bButtonComponent.pressed) {
             console.log("B Button Pressed");
           }
--- src/lab-shared/LabRoom.js
+++ src/lab-shared/LabRoom.js
@@ -16,22 +16,28 @@ const addLabRoom = (scene) => {
   groundMaterial.lineColor = new BABYLON.Color3(1.0, 1.0, 1.0);
   groundMaterial.opacity = 0.98;
   ground.material = groundMaterial;
+  ground.checkCollisions = true;
+
+  // Note: the rotation of these elements is set to put the face of the plane/ground facing the inside of the room so that collisions will work.
 
   const wall1 = BABYLON.MeshBuilder.CreateGround("wall1", { height: 10, width: 20, subdivisions: 4 }, scene);
-  wall1.rotation = new BABYLON.Vector3(Math.PI / 2, 0, Math.PI / 2);
+  wall1.rotation = new BABYLON.Vector3(Math.PI / 2, Math.PI, Math.PI / 2);
   wall1.position = new BABYLON.Vector3(-10, 5, 0);
   wall1.material = groundMaterial;
+  wall1.sideOrientation = "DOUBLESIDE";
+  wall1.checkCollisions = true;
 
   const wall2 = wall1.clone("wall2");
-  wall2.rotation = new BABYLON.Vector3(Math.PI / 2, 0, Math.PI / 2);
+  wall2.rotation.z = -Math.PI / 2;
   wall2.position = new BABYLON.Vector3(10, 5, 0);
 
-  const wall3 = wall1.clone("wall2");
+  console.log("wall rotation", wall1.rotation, wall2.rotation);
+  const wall3 = wall1.clone("wall3");
   wall3.rotation = new BABYLON.Vector3(Math.PI / 2, Math.PI / 2, Math.PI / 2);
   wall3.position = new BABYLON.Vector3(0, 5, -10);
 
-  const wall4 = wall1.clone("wall4");
-  wall4.rotation = new BABYLON.Vector3(Math.PI / 2, Math.PI / 2, Math.PI / 2);
+  const wall4 = wall3.clone("wall4");
+  wall4.rotation.z = -Math.PI / 2;
   wall4.position = new BABYLON.Vector3(0, 5, 10);
 
   // Return the ground to use for teleportation
