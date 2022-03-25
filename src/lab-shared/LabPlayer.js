412d819b9c06643cb1cf6c8db8f6ea32d0e2877e
Joe Simpson <vrhermit@gmail.com>
2022-03-25 14:15:38 -0400

    Loading and using locomotion data for all labs that use the LabPlayer

--- src/lab-shared/LabPlayer.js
+++ src/lab-shared/LabPlayer.js
@@ -9,9 +9,50 @@ Current features
 
 // import * as BABYLON from "babylonjs";
 // import { ref, reactive, watch } from "@vue/runtime-core");
+import * as BABYLON from "babylonjs";
 import { createLabMenu } from "../lab-shared/LabMenu";
+import { reactive } from "@vue/runtime-core";
+import { useStorage } from "@vueuse/core";
+import LabColors from "../lab-shared/LabColors";
 
 export const createLabPlayer = async (scene, teleportMeshes) => {
+  // Default settings for the movement controls
+  const defaultMovementSettings = {
+    locomotionType: "teleport", // "teleport" or "movement"
+    // Movement settings
+    movementEnabled: true,
+    movementSpeed: 0.5, // 1 is too fast most of the time
+    rotationEnabled: true,
+    rotationSpeed: 0.25,
+    // Teleport settings
+    parabolicCheckRadius: 5,
+    rotationAngle: 0.25, // teleport rotation angle, not movement controls
+    backwardsTeleportationDistance: 0.7
+  };
+
+  // Spread the default settings into the stored settings
+  // This will only be set if the local storage value is not found, else it will use the local storage value
+  let storedMovementSettings = useStorage("lab-locomotion-config", {
+    ...defaultMovementSettings
+  });
+
+  // Map the stored settings to the reactive settings object
+  // I could not find a way to get Watch working with useStorage
+  // I don't actually need this to be reactive, but I'm keeping it for now to match the usage on Lab 026
+  let movementSettings = reactive({
+    locomotionType: storedMovementSettings.value.locomotionType,
+    movementEnabled: storedMovementSettings.value.movementEnabled,
+    movementSpeed: storedMovementSettings.value.movementSpeed,
+
+    rotationEnabled: storedMovementSettings.value.rotationEnabled,
+    rotationSpeed: storedMovementSettings.value.rotationSpeed,
+
+    parabolicCheckRadius: storedMovementSettings.value.parabolicCheckRadius,
+    rotationAngle: storedMovementSettings.value.rotationAngle,
+    backwardsTeleportationDistance: storedMovementSettings.value.backwardsTeleportationDistance
+  });
+
+  console.log("storedMovementSettings", storedMovementSettings);
   const { toggleMenu } = createLabMenu(scene);
   // Create the default experience
   let xr = await scene.createDefaultXRExperienceAsync({
@@ -25,6 +66,89 @@ export const createLabPlayer = async (scene, teleportMeshes) => {
   xr.baseExperience.onInitialXRPoseSetObservable.add((xrCamera) => {
     xrCamera.position.z = -2;
   });
+
+  const setupCameraForCollisions = (camera) => {
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
+    camera.ellipsoid = new BABYLON.Vector3(0.7, 1, 0.7);
+    camera.ellipsoidOffset = new BABYLON.Vector3(0, 0.5, 0);
+  };
+
+  const useMovementControls = (featureManager) => {
+    // Turn off the other feature
+    featureManager.disableFeature(BABYLON.WebXRFeatureName.TELEPORTATION);
+    // Configure and enable the movement controls
+    const swappedHandednessConfiguration = [
+      {
+        allowedComponentTypes: [BABYLON.WebXRControllerComponent.THUMBSTICK_TYPE, BABYLON.WebXRControllerComponent.TOUCHPAD_TYPE],
+        forceHandedness: "right",
+        axisChangedHandler: (axes, movementState, featureContext, xrInput) => {
+          console.log(xrInput);
+          movementState.rotateX = Math.abs(axes.x) > featureContext.rotationThreshold ? axes.x : 0;
+          movementState.rotateY = Math.abs(axes.y) > featureContext.rotationThreshold ? axes.y : 0;
+        }
+      },
+      {
+        allowedComponentTypes: [BABYLON.WebXRControllerComponent.THUMBSTICK_TYPE, BABYLON.WebXRControllerComponent.TOUCHPAD_TYPE],
+        forceHandedness: "left",
+        axisChangedHandler: (axes, movementState, featureContext, xrInput) => {
+          console.log(xrInput);
+          movementState.moveX = Math.abs(axes.x) > featureContext.movementThreshold ? axes.x : 0;
+          movementState.moveY = Math.abs(axes.y) > featureContext.movementThreshold ? axes.y : 0;
+        }
+      }
+    ];
+
+    setupCameraForCollisions(xr.input.xrCamera);
+
+    featureManager.enableFeature(BABYLON.WebXRFeatureName.MOVEMENT, "latest", {
+      xrInput: xr.input,
+      customRegistrationConfigurations: swappedHandednessConfiguration,
+      movementEnabled: movementSettings.movementEnabled,
+      movementSpeed: movementSettings.movementSpeed,
+      rotationEnabled: movementSettings.rotationEnabled,
+      rotationSpeed: movementSettings.rotationSpeed
+    });
+  };
+
+  const useTeleportControls = (featureManager) => {
+    // Turn off the other feature
+    featureManager.disableFeature(BABYLON.WebXRFeatureName.MOVEMENT);
+    // Configure and enable the teleportation feature
+    const createTeleportationSetup = () => {
+      const teleportRingMat = new BABYLON.StandardMaterial("grab-mat1", scene);
+      teleportRingMat.diffuseColor = LabColors["light1"];
+      teleportRingMat.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+
+      let setup = {
+        xrInput: xr.input,
+        floorMeshes: teleportMeshes
+      };
+
+      setup["defaultTargetMeshOptions"] = {
+        teleportationFillColor: "#3e4a5d",
+        teleportationBorderColor: "#8854d0",
+        torusArrowMaterial: teleportRingMat
+      };
+
+      setup["renderingGroupId"] = 1;
+
+      return setup;
+    };
+
+    const teleportControlManager = featureManager.enableFeature(BABYLON.WebXRFeatureName.TELEPORTATION, "stable", createTeleportationSetup({}));
+    teleportControlManager.parabolicCheckRadius = movementSettings.parabolicCheckRadius;
+    teleportControlManager.rotationAngle = movementSettings.rotationAngle;
+    teleportControlManager.backwardsTeleportationDistance = movementSettings.backwardsTeleportationDistance;
+    teleportControlManager.rotationEnabled = false; // rotation while teleport is disabled
+  };
+
+  const featureManager = xr.baseExperience.featuresManager;
+  if (movementSettings.locomotionType === "movement") {
+    useMovementControls(featureManager);
+  } else {
+    useTeleportControls(featureManager);
+  }
 
   //controller input
   xr.input.onControllerAddedObservable.add((controller) => {
